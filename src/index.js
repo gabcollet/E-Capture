ff4ba389b55ce9239a37dcdd7acaf1a88bcb6a21
gabcollet <gab_collet@hotmail.com>
2022-11-03 18:21:01 -0400

    work but no save

---
+++ src/index.js
@@ -0,0 +1,89 @@
+const { app, BrowserWindow, desktopCapturer, ipcMain } = require('electron');
+const remoteMain = require('@electron/remote/main');
+remoteMain.initialize();
+const path = require('path');
+
+let mainWindow = null;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'script.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+  
+  remoteMain.enable(mainWindow.webContents)
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+async function capture (sourceName) {
+  const sources = await desktopCapturer
+    .getSources({
+      types: ['window'],
+      thumbnailSize: { width: 1920,height: 1080 },
+    })
+  let image = null;
+  for (let i = 0; sources[i]; i++){
+    if (sources[i].name === sourceName){
+      image = sources[i].thumbnail.toDataURL();
+      break;
+    }
+  }
+  mainWindow.webContents.send('screenshot:captured', image);
+}
+
+async function getSourcesList () {
+  const sources = await desktopCapturer
+    .getSources({
+      types: ['window'],
+      thumbnailSize: { width: 1920,height: 1080 },
+    })
+  mainWindow.webContents.send('videoList', sources);
+}
+
+ipcMain.on('screenshot:capture', (e, value) => {
+  capture(value.sourceName);
+})
+
+ipcMain.on('imgSource', (e, value) => {
+  getSourcesList();
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/script.js
@@ -0,0 +1,52 @@
+const { ipcRenderer } = require("electron");
+const remote = require('@electron/remote');
+const { Menu } = remote;
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded');
+    let sourceName = '';
+    let image = null;
+
+    //Send event for the capture and create save button 
+    document.getElementById('btn').addEventListener('click', () => {
+        ipcRenderer.send('screenshot:capture', {sourceName});
+        // const button = document.createElement("button");
+        // button.innerText = "Save";
+        // button.id = "saveBtn";
+        // document.getElementsByTagName("body")[0].appendChild(button);  
+    });
+
+    //Send event for Select
+    document.getElementById('screenSelectBtn').addEventListener('click', () => {
+        ipcRenderer.send('imgSource', {});
+    });
+    
+    //Send event for Save
+    document.getElementById('saveBtn').addEventListener('click', () => {
+        ipcRenderer.send('imgSource', {});
+    });
+
+    ipcRenderer.on('screenshot:captured', (e, imageData) => {
+        document.getElementById("placeholder").src = imageData;
+        image = imageData;
+    });
+    
+    let srcOptionMenu;
+    ipcRenderer.on('videoList', (e, sources) => {
+        srcOptionMenu = Menu.buildFromTemplate(
+            sources.map(source => {
+                return {
+                    label: source.name,
+                    click: () => selectSource(source)
+                };
+            })
+        );
+        srcOptionMenu.popup();
+    });
+    
+    function selectSource (source) {
+        console.log(source.name);
+        document.getElementById("txt").innerText = source.name;
+        sourceName = source.name;
+    }
+});
